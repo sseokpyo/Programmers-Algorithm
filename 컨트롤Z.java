08ab8fd27ef3c6288e286fa3485dec13b1050b2c
P.yo_ <87968279+sseokpyo@users.noreply.github.com>
2023-12-08 19:00:41 +0900

    programers algorithm
    java lv 0

---
+++ 컨트롤Z.java
@@ -0,0 +1,17 @@
+/*
+숫자와 "Z"가 공백으로 구분되어 담긴 문자열이 주어집니다. 문자열에 있는 숫자를 차례대로 더하려고 합니다. 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다는 뜻입니다. 숫자와 "Z"로 이루어진 문자열 s가 주어질 때, 머쓱이가 구한 값을 return 하도록 solution 함수를 완성해보세요.
+*/
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] array = s.split(" ");
+        for(int i = 0; i < array.length; i ++){
+            if(i > 0 && array[i].equals("Z")){
+                answer = answer - Integer.valueOf(array[i-1]);
+            } else {
+                answer += Integer.valueOf(array[i]);
+            }
+        }
+        return answer;
+    }
+}
