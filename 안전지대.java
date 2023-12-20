bf8a4602bcfd494885afb08b1a01323783e56bd0
P.yo_ <87968279+sseokpyo@users.noreply.github.com>
2023-12-20 18:19:32 +0900

    programers algorithm
    java lv0

---
+++ 안전지대.java
@@ -0,0 +1,58 @@
+/*
+다음 그림과 같이 지뢰가 있는 지역과 지뢰에 인접한 위, 아래, 좌, 우 대각선 칸을 모두 위험지역으로 분류합니다.
+
+_|_|_|_|_
+_|x|x|x|_
+_|x|o|x|_
+_|x|x|x|_
+_|_|_|_|_
+지뢰는 2차원 배열 board에 1로 표시되어 있고 board에는 지뢰가 매설 된 지역 1과, 지뢰가 없는 지역 0만 존재합니다.
+지뢰가 매설된 지역의 지도 board가 매개변수로 주어질 때, 안전한 지역의 칸 수를 return하도록 solution 함수를 완성해주세요.
+*/
+
+class Solution {
+    public int solution(int[][] board) {
+        int answer = 0;
+        int l = board.length;
+        int[][] nb = new int[l+2][l+2];
+        
+        for(int x = 0; x < l; x++){
+            for(int y = 0; y < l; y++){
+                if(board[y][x] == 1){                    
+                    nb[y+1][x+1] = 1; 
+                    System.out.println(x + " || " + y);
+                }
+            }
+        }
+        
+        for(int y = 0; y < l; y++){
+            for(int x = 0; x < l; x++){
+                if(board[y][x] == 1){                    
+                    nb[y][x] = 1;
+                    nb[y][x+1] = 1;
+                    nb[y][x+2] = 1;
+                    
+                    nb[y+1][x] = 1;
+                    nb[y+1][x+1] = 1;
+                    nb[y+1][x+2] = 1;
+                    
+                    nb[y+2][x] = 1;
+                    nb[y+2][x+1] = 1;
+                    nb[y+2][x+2] = 1;
+                }
+            }
+        }
+        
+        
+        
+        for(int y = 1; y < l+1; y++){
+            for(int x = 1; x < l+1; x++){
+                if(nb[y][x] == 0){
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
