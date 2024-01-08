fd1b1aa44935e162ef97d51253f09d0923a659b8
P.yo_ <87968279+sseokpyo@users.noreply.github.com>
2024-01-08 14:04:30 +0900

    programers_algorithm
    java lv 0

---
+++ 로그인 성공.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        System.out.println(db.length);
+        for(int i = 0; i < db.length; i ++){
+            if(id_pw[0].equals(db[i][0])){
+                if(id_pw[1].equals(db[i][1])){
+                    answer = "login";
+                    break;
+                } else {
+                    answer = "wrong pw";
+                    break;
+                }
+            } else {
+                answer = "fail";
+            }
+        }
+        return answer;
+    }
+}
